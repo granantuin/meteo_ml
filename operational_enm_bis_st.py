93b839062a46f052c6decfbbd20df29ed3d649c3
granantuin <49106818+granantuin@users.noreply.github.com>
2022-02-22 10:11:00 +0100

    Update operational_enm_bis_st.py

--- operational_enm_bis_st.py
+++ operational_enm_bis_st.py
@@ -112,7 +112,7 @@ model_x_var_p=meteo_model[23:47][algo_prec_d1["x_var"]]
 #forecast machine learning wind precipitation
 prec_ml=algo_prec_d1["ml_model"].predict_proba(model_x_var_p)
 df_show_pre=pd.DataFrame(prec_ml,columns=["no p","precipitación"])["precipitación"].map(lambda n: '{:.0%}'.format(n))
-df_show_pre["Hora UTC"]=meteo_model.index[23:47]
+#df_show_pre["Hora UTC"]=meteo_model.index[23:47]
 st.title(""" Probabilidad precipitación ENM mañana con Machine Learning""")
-#st.dataframe(df_show_pre)
+st.dataframe(df_show_pre)
 #st.dataframe(meteo_model)
